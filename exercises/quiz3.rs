7e4e13d91d515372f5298f639a3126778d4d7028
se-it-z <phlippseitz@gmail.com>
2023-03-18 10:23:13 +0100

    Solutions

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -14,10 +14,25 @@
 
 // Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+use std::fmt;
+
+pub enum Grade {
+    FloatType(f32),
+    StrType(String)
+}
+
+impl fmt::Display for Grade {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Grade::FloatType(fVar) => write!(f, "{}", fVar),
+            Grade::StrType(strVar) => write!(f, "{}", strVar),
+            _ => Err(std::fmt::Error)
+        }
+    }
+}
 
 pub struct ReportCard {
-    pub grade: f32,
+    pub grade: Grade,
     pub student_name: String,
     pub student_age: u8,
 }
@@ -36,7 +51,7 @@ mod tests {
     #[test]
     fn generate_numeric_report_card() {
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: Grade::FloatType(2.1),
             student_name: "Tom Wriggle".to_string(),
             student_age: 12,
         };
@@ -50,7 +65,7 @@ mod tests {
     fn generate_alphabetic_report_card() {
         // TODO: Make sure to change the grade here after you finish the exercise.
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: Grade::StrType(String::from("A+")),
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
         };
